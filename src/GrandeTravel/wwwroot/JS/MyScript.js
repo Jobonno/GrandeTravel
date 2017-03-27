0798f54ab83f4e78410addbb08840bfe171347ea
Jobonno <jobonno@gmail.com>
2017-03-27 12:19:28 +1100

    Add NavBar Make Responsive

--- src/GrandeTravel/wwwroot/JS/MyScript.js
+++ src/GrandeTravel/wwwroot/JS/MyScript.js
@@ -2,9 +2,10 @@
     var title = $("Title").text();
     if (title === "Grande Travel Landing Page") {
         $("body").css("backgroundColor", "#000");
-    
-
-                
+        $("footer").addClass("hide");             
       
     }
+    //$("i.fa").hover(function () {
+    //    $(this).css("fontSize", "27px")
+    //});
 });
