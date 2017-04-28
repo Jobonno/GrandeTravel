199f63f5395b57e9eb19ab0cd5908504e58441b5
Joe <Joe@JOE-PC>
2017-04-28 19:01:29 +1000

    Add LogOut

--- src/GrandeTravel/Controllers/AccountController.cs
+++ src/GrandeTravel/Controllers/AccountController.cs
@@ -88,5 +88,12 @@ namespace GrandeTravel.Controllers
             return View(vm);
         }
 
+        [HttpPost]        
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
