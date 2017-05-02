9cabf60bdbc92d508fcafc35c1c3c9fb5d70c7f1
Joe <Joe@JOE-PC>
2017-05-02 17:39:48 +1000

    Create Booking Use Case

---
+++ src/GrandeTravel/Controllers/BookingController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GrandeTravel.Services;
+using GrandeTravel.Models;
+using GrandeTravel.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GrandeTravel.Controllers
+{
+    public class BookingController : Controller
+    {
+        private IRepository<Booking> _bookingRepo;
+        private UserManager<MyUser> _userManager;
+
+        public BookingController(IRepository<Booking> bookingRepo, UserManager<MyUser> userManager)
+        {
+            _userManager = userManager;
+            _bookingRepo = bookingRepo;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create(int id)
+        {
+            var userId = _userManager.GetUserId(User);
+            CreateBookingViewModel vm = new CreateBookingViewModel
+            {
+                TravelPackageId = id,
+                MyUserId = userId
+            };
+            return View(vm);
+        }
+        [HttpPost]
+        public IActionResult Create(CreateBookingViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                Booking booking = new Booking
+                {
+                    BookingDate = vm.BookingDate,
+                    TravelPackageId = vm.TravelPackageId,
+                    MyUserId = vm.MyUserId
+                };
+                _bookingRepo.Create(booking);
+                return RedirectToAction("Index", "TravelPackage");
+            }
+            return View(vm);
+        }
+    }
+}
---
+++ src/GrandeTravel/ViewModels/CreateBookingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrandeTravel.ViewModels
+{
+    public class CreateBookingViewModel
+    {
+        
+        [Display(Name = "Booking Date")]
+        [DataType(DataType.Date)]
+        public DateTime BookingDate { get; set; }
+
+        public int TravelPackageId { get; set; }
+
+        public string MyUserId { get; set; }
+    }
+}
