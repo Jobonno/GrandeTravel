5819af0243a1e0c2a02a05d114c3b138f73c7d7f
Joe <Joe@JOE-PC>
2017-05-08 14:40:15 +1000

    Add Contact Form Submission Functionality, also Styling to Details Page

--- src/GrandeTravel/Controllers/HomeController.cs
+++ src/GrandeTravel/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GrandeTravel.ViewModels;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,9 +25,37 @@ namespace GrandeTravel.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Contact()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(vm.FromAddress, vm.FromAddress));
+                message.To.Add(new MailboxAddress("Joe", "jobonno@gmail.com"));
+                message.Subject = vm.Subject;
+                message.Body = new TextPart("plain")
+                {
+                    Text = vm.Body
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate("jobonno@gmail.com", "monteleone82");
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+                return RedirectToAction("index");
+            }
+            return View(vm);
         }
 
     }
---
+++ src/GrandeTravel/ViewModels/ContactViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrandeTravel.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required, DataType(DataType.EmailAddress), Display(Name ="Email Address")]
+        public string FromAddress { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
